0a54930e370b8c497090a5570e8d2bcc7d8284ba
dotslashed <73475291+dotslashed@users.noreply.github.com>
2021-06-13 21:25:54 +0530

    Update subtake.sh

--- subtake.sh
+++ subtake.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cat sub_domains.txt | while read dom do; do host $dom | grep  "alias" | awk '{print $NF}';done | while read cnams do; do host $cnams | if grep -qo "has address";then echo NOT_VULNERABLE; else echo $cnam--for--$dom--DIDNO
+FILE_NAME=$1
+
+cat FILE_NAME | while read dom do; do host $dom | grep  "alias" | awk '{print $NF}';done | while read cnams do; do host $cnams | if grep -qo "has address";then echo NOT_VULNERABLE; else echo $cnam--for--$dom--DIDNO
 T_RESOLVE; fi; done
